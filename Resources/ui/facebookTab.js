03ac669676a491ef6aa533ec6a5dd2b6842216fb
hiroshi oyamada <h5y1m141@gmail.com>
2013-07-01 12:51:46 +0900

    Facebookのユーザ情報を取得してTableViewに配置するための_userSection()というメソッド実装

--- Resources/ui/facebookTab.js
+++ Resources/ui/facebookTab.js
@@ -3,7 +3,8 @@ var facebookTab;
 facebookTab = (function() {
 
   function facebookTab() {
-    var baseColor, button, facebookWindowTitle, fb, fbLoginButton;
+    var baseColor, button, facebookWindowTitle, fb, fbLoginButton,
+      _this = this;
     baseColor = {
       barColor: "#f9f9f9",
       backgroundColor: "#dfdfdf",
@@ -15,7 +16,10 @@ facebookTab = (function() {
     fb.forceDialogAuth = false;
     cbFan.facebookToken = fb.accessToken;
     fb.addEventListener('login', function(e) {
+      var that;
+      that = _this;
       if (e.success) {
+        alert(that);
         return Cloud.SocialIntegrations.externalAccountLogin({
           type: "facebook",
           token: fb.accessToken
@@ -109,6 +113,64 @@ facebookTab = (function() {
     json = JSON.parse(file);
     appid = json.facebook.appid;
     return appid;
+  };
+
+  facebookTab.prototype._userSection = function(user) {
+    var baseColor, menuHeaderTitle, menuHeaderView, menuSection, nameLabel, nameRow, rows, table;
+    baseColor = {
+      barColor: "#f9f9f9",
+      backgroundColor: "#dfdfdf",
+      keyColor: "#EDAD0B"
+    };
+    rows = [];
+    table = Ti.UI.createTableView({
+      backgroundColor: baseColor.backgroundColor,
+      style: Titanium.UI.iPhone.TableViewStyle.GROUPED,
+      width: 'auto',
+      height: '100',
+      top: 50,
+      left: 0
+    });
+    menuHeaderView = Ti.UI.createView({
+      backgroundColor: baseColor.backgroundColor,
+      height: 30
+    });
+    menuHeaderTitle = Ti.UI.createLabel({
+      top: 0,
+      left: 5,
+      color: '#333',
+      font: {
+        fontSize: 18,
+        fontFamily: 'Rounded M+ 1p'
+      },
+      text: 'アカウント情報'
+    });
+    menuHeaderView.add(menuHeaderTitle);
+    menuSection = Ti.UI.createTableViewSection({
+      headerView: menuHeaderView
+    });
+    nameRow = Ti.UI.createTableViewRow({
+      backgroundColor: baseColor.backgroundColor,
+      height: 40,
+      className: "facebook"
+    });
+    nameLabel = Ti.UI.createLabel({
+      text: user.first_name + user.last_name,
+      width: 280,
+      color: "#333",
+      left: 5,
+      top: 5,
+      font: {
+        fontSize: 18,
+        fontFamily: 'Rounded M+ 1p',
+        fontWeight: 'bold'
+      }
+    });
+    nameRow.add(nameLabel);
+    menuSection.add(nameRow);
+    rows.push(menuSection);
+    table.setData(rows);
+    return table;
   };
 
   return facebookTab;
--- coffee/ui/facebookTab.coffee
+++ coffee/ui/facebookTab.coffee
@@ -11,8 +11,10 @@ class facebookTab
     fb.forceDialogAuth = false
     cbFan.facebookToken = fb.accessToken
       
-    fb.addEventListener('login', (e) ->
+    fb.addEventListener('login', (e) =>
+      that = @
       if e.success
+        alert that
         Cloud.SocialIntegrations.externalAccountLogin
           type: "facebook"
           token: fb.accessToken
@@ -90,5 +92,58 @@ class facebookTab
     json = JSON.parse(file)
     appid = json.facebook.appid
     return appid
+  _userSection:(user) ->
+    baseColor =
+      barColor:"#f9f9f9"
+      backgroundColor:"#dfdfdf"
+      keyColor:"#EDAD0B"
+
+    rows = []
+    table = Ti.UI.createTableView
+      backgroundColor: baseColor.backgroundColor
+      style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+      width:'auto'
+      height:'100'
+      top:50
+      left:0
+      
+    menuHeaderView = Ti.UI.createView
+      backgroundColor:baseColor.backgroundColor
+      height:30
+      
+    menuHeaderTitle = Ti.UI.createLabel
+      top:0
+      left:5
+      color:'#333'
+      font:
+        fontSize:18
+        fontFamily :'Rounded M+ 1p'
+      text:'アカウント情報'
+      
+    menuHeaderView.add menuHeaderTitle
     
+    menuSection = Ti.UI.createTableViewSection
+      headerView:menuHeaderView
+      
+    nameRow = Ti.UI.createTableViewRow
+      backgroundColor:baseColor.backgroundColor
+      height:40
+      className:"facebook"
+    nameLabel = Ti.UI.createLabel
+      text: user.first_name + user.last_name
+      width:280
+      color:"#333"
+      left:5
+      top:5
+      font:
+        fontSize:18
+        fontFamily :'Rounded M+ 1p'
+        fontWeight:'bold'  
+    nameRow.add nameLabel      
+    menuSection.add nameRow
+
+    rows.push menuSection
+    
+    table.setData rows
+    return table        
 module.exports =  facebookTab
