ea7dcb77087d82356475b28d5738a5153863c130
hiroshi oyamada <h5y1m141@gmail.com>
2013-07-07 08:34:21 +0900

    コメントとrating含めてお気に入り登録機能実装

--- Resources/ui/shopDataDetail.js
+++ Resources/ui/shopDataDetail.js
@@ -38,7 +38,7 @@ shopDataDetail = (function() {
       borderRadius: 0,
       color: '#fff',
       font: {
-        fontSize: 32,
+        fontSize: 24,
         fontFamily: 'FontAwesome'
       },
       title: String.fromCharCode("0xf095")
@@ -60,51 +60,189 @@ shopDataDetail = (function() {
       height: 40,
       selectedColor: 'transparent'
     });
-    this.memoIcon = Ti.UI.createButton({
+    this.starIcon = Ti.UI.createButton({
       top: 5,
       left: 10,
       width: 30,
       height: 30,
-      backgroundColor: "#3261AB",
+      backgroundColor: "#DA5019",
       backgroundImage: "NONE",
       borderWidth: 0,
       borderRadius: 0,
       color: '#fff',
       font: {
-        fontSize: 32,
+        fontSize: 28,
         fontFamily: 'LigatureSymbols'
       },
-      title: String.fromCharCode("0xe08d")
+      title: String.fromCharCode("0xe041")
     });
-    this.memoIcon.addEventListener('click', function(e) {
-      cbFan.activityIndicator.show();
-      return Cloud.Places.query({
-        page: 1,
-        per_page: 1,
-        where: {
-          name: e.source.shopName
+    this.starIcon.addEventListener('click', function(e) {
+      var ActivityIndicator, activityIndicator, addNewIcon, closeButton, contents, disableColor, enableColor, i, label, leftPostion, modalWindow, ratings, shopName, starIcon, textArea, _i, _winTitle;
+      modalWindow = Ti.UI.createWindow({
+        backgroundColor: "#f3f3f3",
+        barColor: "#f9f9f9"
+      });
+      closeButton = Titanium.UI.createButton({
+        backgroundImage: "ui/image/backButton.png",
+        width: 44,
+        height: 44
+      });
+      closeButton.addEventListener('click', function(e) {
+        return modalWindow.close({
+          animated: true
+        });
+      });
+      ActivityIndicator = require("ui/activityIndicator");
+      activityIndicator = new ActivityIndicator();
+      modalWindow.leftNavButton = closeButton;
+      modalWindow.add(activityIndicator);
+      _winTitle = Ti.UI.createLabel({
+        textAlign: 'center',
+        color: '#333',
+        font: {
+          fontSize: 18,
+          fontFamily: 'Rounded M+ 1p',
+          fontWeight: 'bold'
+        },
+        text: "お気に入り登録:" + e.source.shopName
+      });
+      if (Ti.Platform.osname === 'iphone') {
+        modalWindow.setTitleControl(_winTitle);
+      }
+      enableColor = "#FFE600";
+      disableColor = "#FFFBD5";
+      ratings = 0;
+      leftPostion = [5, 45, 85, 125, 165];
+      for (i = _i = 0; _i <= 4; i = ++_i) {
+        starIcon = Ti.UI.createButton({
+          top: 5,
+          left: leftPostion[i],
+          width: 30,
+          height: 30,
+          selected: false,
+          backgroundColor: disableColor,
+          backgroundImage: "NONE",
+          borderWidth: 0,
+          borderRadius: 5,
+          color: '#fff',
+          font: {
+            fontSize: 24,
+            fontFamily: 'LigatureSymbols'
+          },
+          title: String.fromCharCode("0xe121")
+        });
+        starIcon.addEventListener('click', function(e) {
+          if (e.source.selected === false) {
+            e.source.backgroundColor = enableColor;
+            e.source.selected = true;
+            return ratings++;
+          } else {
+            e.source.backgroundColor = disableColor;
+            e.source.selected = false;
+            return ratings--;
+          }
+        });
+        modalWindow.add(starIcon);
+      }
+      label = Ti.UI.createLabel({
+        text: "登録したくなった理由をメモに残しておきましょう!",
+        width: 300,
+        height: 40,
+        color: "#333",
+        left: 10,
+        top: 55,
+        font: {
+          fontSize: 14,
+          fontFamily: 'Rounded M+ 1p',
+          fontWeight: 'bold'
         }
-      }, function(e) {
-        var id;
-        if (e.success) {
-          id = e.places[0].id;
-          return Cloud.Reviews.create({
-            rating: 1,
-            place_id: id,
-            custom_fields: {
-              place_id: id
-            }
-          }, function(e) {
-            cbFan.activityIndicator.hide();
-            if (e.success) {
-              return alert("お気に入りに登録しました");
-            } else {
-              return alert("お気に入りに登録することができませんでした");
-            }
-          });
-        } else {
-          return Ti.API.info("Error:\n");
-        }
+      });
+      label.addEventListener('click', function(e) {});
+      contents = "";
+      shopName = e.source.shopName;
+      textArea = Titanium.UI.createTextArea({
+        value: '',
+        height: 150,
+        width: 300,
+        top: 100,
+        left: 5,
+        font: {
+          fontSize: 12,
+          fontFamily: 'Rounded M+ 1p',
+          fontWeight: 'bold'
+        },
+        color: '#222',
+        textAlign: 'left',
+        borderWidth: 2,
+        borderColor: "#dfdfdf",
+        borderRadius: 5,
+        keyboardType: Titanium.UI.KEYBOARD_DEFAULT
+      });
+      textArea.addEventListener('return', function(e) {
+        contents = e.value;
+        Ti.API.info("e.value is " + e.value);
+        return textArea.blur();
+      });
+      addNewIcon = Ti.UI.createButton({
+        top: 270,
+        left: 110,
+        width: 100,
+        height: 40,
+        backgroundColor: "#DA5019",
+        backgroundImage: "NONE",
+        borderWidth: 0,
+        borderRadius: 5,
+        color: '#fff',
+        font: {
+          fontSize: 32,
+          fontFamily: 'LigatureSymbols'
+        },
+        title: String.fromCharCode("0xe041")
+      });
+      modalWindow.add(addNewIcon);
+      modalWindow.add(textArea);
+      modalWindow.add(label);
+      addNewIcon.addEventListener('click', function(e) {
+        activityIndicator.show();
+        Ti.API.info("contents is " + contents);
+        ratings = ratings;
+        contents = contents;
+        return Cloud.Places.query({
+          page: 1,
+          per_page: 1,
+          where: {
+            name: shopName
+          }
+        }, function(e) {
+          var id;
+          if (e.success) {
+            id = e.places[0].id;
+            Ti.API.info("id is " + id);
+            return Cloud.Reviews.create({
+              rating: ratings,
+              content: contents,
+              place_id: id,
+              custom_fields: {
+                place_id: id
+              }
+            }, function(e) {
+              activityIndicator.hide();
+              if (e.success) {
+                return alert("お気に入りに登録しました");
+              } else {
+                alert(e);
+                return alert("すでにお気に入りに登録されているか\nサーバーがダウンしているために登録することができませんでした");
+              }
+            });
+          } else {
+            return Ti.API.info("Error:\n");
+          }
+        });
+      });
+      return modalWindow.open({
+        modal: true,
+        modalTransitionStyle: Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+        modalStyle: Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET
       });
     });
     this.editLabel = Ti.UI.createLabel({
@@ -122,7 +260,7 @@ shopDataDetail = (function() {
     addressRow.add(this.addressLabel);
     phoneRow.add(this.phoneIcon);
     phoneRow.add(this.phoneLabel);
-    reviewRow.add(this.memoIcon);
+    reviewRow.add(this.starIcon);
     reviewRow.add(this.editLabel);
     shopData.push(this.section);
     shopData.push(addressRow);
@@ -158,7 +296,7 @@ shopDataDetail = (function() {
       fontFamily: 'LigatureSymbols'
     });
     shopName = data.name;
-    this.memoIcon.shopName = shopName;
+    this.starIcon.shopName = shopName;
     this.editLabel.setFont({
       fontFamily: 'Rounded M+ 1p'
     });
--- coffee/ui/shopDataDetail.coffee
+++ coffee/ui/shopDataDetail.coffee
@@ -36,7 +36,7 @@ class shopDataDetail
       borderRadius:0
       color:'#fff'      
       font:
-        fontSize: 32
+        fontSize: 24
         fontFamily:'FontAwesome'
       title:String.fromCharCode("0xf095")
       
@@ -58,43 +58,186 @@ class shopDataDetail
       height:40
       selectedColor:'transparent'
       
-    @memoIcon = Ti.UI.createButton
+    @starIcon = Ti.UI.createButton
       top:5
       left:10
       width:30
       height:30
-      backgroundColor:"#3261AB"
+      backgroundColor:"#DA5019"
       backgroundImage:"NONE"
       borderWidth:0
       borderRadius:0
       color:'#fff'      
       font:
-        fontSize: 32
+        fontSize: 28
         fontFamily:'LigatureSymbols'
-      title:String.fromCharCode("0xe08d")
+      title:String.fromCharCode("0xe041")
       
-    @memoIcon.addEventListener('click',(e) ->
-      cbFan.activityIndicator.show()
-      Cloud.Places.query
-        page: 1
-        per_page: 1
-        where:{name:e.source.shopName}
-      , (e) ->
-        if e.success
-          id = e.places[0].id
-          Cloud.Reviews.create
-            rating: 1
-            place_id:id
-            custom_fields:
+    @starIcon.addEventListener('click',(e) ->
+      modalWindow = Ti.UI.createWindow
+        backgroundColor:"#f3f3f3"
+        barColor:"#f9f9f9"
+        
+      closeButton = Titanium.UI.createButton
+        backgroundImage:"ui/image/backButton.png"
+        width:44
+        height:44
+        
+        
+      closeButton.addEventListener('click',(e) ->
+        return modalWindow.close({animated:true})
+      )
+      ActivityIndicator = require("ui/activityIndicator")
+      activityIndicator = new ActivityIndicator()
+      modalWindow.leftNavButton = closeButton
+      modalWindow.add activityIndicator
+      
+      _winTitle = Ti.UI.createLabel
+        textAlign: 'center'
+        color:'#333'
+        font:
+          fontSize:18
+          fontFamily : 'Rounded M+ 1p'
+          fontWeight:'bold'
+        text:"お気に入り登録:#{e.source.shopName}"
+        
+      if Ti.Platform.osname is 'iphone'  
+        modalWindow.setTitleControl _winTitle
+      
+      # ratingsを１から５までで管理できるので星印を使って表現
+      # 選択されていない状態を意図するのがdisableColorとする
+      enableColor  = "#FFE600"
+      disableColor = "#FFFBD5"
+      ratings = 0
+      leftPostion = [5, 45, 85, 125, 165]
+      for i in [0..4]
+
+        starIcon = Ti.UI.createButton
+          top:5
+          left:leftPostion[i]
+          width:30
+          height:30
+          selected:false
+          backgroundColor:disableColor
+          backgroundImage:"NONE"
+          borderWidth:0
+          borderRadius:5
+          color:'#fff'
+          font:
+            fontSize: 24
+            fontFamily:'LigatureSymbols'
+          title:String.fromCharCode("0xe121")
+          
+        starIcon.addEventListener('click',(e) ->
+          if e.source.selected is false
+            e.source.backgroundColor = enableColor
+            e.source.selected = true
+            ratings++
+          else
+            e.source.backgroundColor = disableColor
+            e.source.selected = false
+            ratings--            
+        )  
+        modalWindow.add starIcon  
+      
+      label = Ti.UI.createLabel
+        text: "登録したくなった理由をメモに残しておきましょう!"
+        width:300
+        height:40
+        color:"#333"
+        left:10
+        top:55
+        font:
+          fontSize:14
+          fontFamily :'Rounded M+ 1p'
+          fontWeight:'bold'
+      label.addEventListener('click',(e) ->
+        # alert ratings
+      )
+      contents = ""
+      shopName = e.source.shopName
+      textArea = Titanium.UI.createTextArea
+        value:''
+        height:150
+        width:300
+        top:100
+        left:5
+        font:
+          fontSize:12
+          fontFamily :'Rounded M+ 1p'
+          fontWeight:'bold'
+        color:'#222',
+        textAlign:'left'
+        borderWidth:2
+        borderColor:"#dfdfdf"
+        borderRadius:5
+        keyboardType:Titanium.UI.KEYBOARD_DEFAULT
+        
+      # 入力完了後、キーボードを消す  
+      textArea.addEventListener('return',(e)->
+        contents = e.value
+        Ti.API.info "e.value is #{e.value}"
+        textArea.blur()
+      )  
+      addNewIcon = Ti.UI.createButton
+        top:270
+        left:110
+        width:100
+        height:40
+        backgroundColor:"#DA5019"
+        backgroundImage:"NONE"
+        borderWidth:0
+        borderRadius:5
+        color:'#fff'
+        font:
+          fontSize: 32
+          fontFamily:'LigatureSymbols'
+        title:String.fromCharCode("0xe041")
+
+        
+      modalWindow.add addNewIcon
+      modalWindow.add textArea
+      modalWindow.add label
+      addNewIcon.addEventListener('click',(e)->
+        activityIndicator.show()
+        # 次のCloud.Places.queryからはaddNewIconの外側にある
+        # 変数参照できないはずなのでここでローカル変数として格納しておく
+        Ti.API.info "contents is #{contents}"
+        ratings = ratings
+        contents = contents
+        Cloud.Places.query
+          page: 1
+          per_page: 1
+          where:{name:shopName}
+        , (e) ->
+          if e.success
+            id = e.places[0].id
+            # Ti.API.info "ratings is #{ratings}"
+            Ti.API.info "id is #{id}"
+            Cloud.Reviews.create
+              rating:ratings
+              content:contents              
               place_id:id
-          , (e) ->
-            cbFan.activityIndicator.hide()
-            if e.success
-              alert "お気に入りに登録しました"
-            else  
-              alert "お気に入りに登録することができませんでした"
-        else
-          Ti.API.info "Error:\n"
+              custom_fields:
+                place_id:id
+            , (e) ->
+              activityIndicator.hide()
+              if e.success
+                alert "お気に入りに登録しました"
+              else
+                alert e
+                alert "すでにお気に入りに登録されているか\nサーバーがダウンしているために登録することができませんでした"
+          else
+            Ti.API.info "Error:\n"        
+      )
+      modalWindow.open(
+        modal:true
+        modalTransitionStyle: Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL
+        modalStyle: Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET
+      )
+      
+      
+
     )
 
 
@@ -112,7 +255,7 @@ class shopDataDetail
     addressRow.add @addressLabel
     phoneRow.add @phoneIcon
     phoneRow.add @phoneLabel
-    reviewRow.add @memoIcon
+    reviewRow.add @starIcon
     reviewRow.add @editLabel
     
     shopData.push @section  
@@ -144,7 +287,7 @@ class shopDataDetail
     @phoneLabel.setText("電話する")
     @editLabel.setFont({fontSize: 32,fontFamily: 'LigatureSymbols'})
     shopName = data.name
-    @memoIcon.shopName =  shopName
+    @starIcon.shopName =  shopName
     @editLabel.setFont({fontFamily :'Rounded M+ 1p'})
     @editLabel.setText("お気に入り登録する")
 
