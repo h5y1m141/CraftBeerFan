70eee04e715aa9d82c30b18d05f327b1c7e4a417
hiroshi oyamada <h5y1m141@gmail.com>
2013-07-20 06:27:40 +0900

    起動時にウィザード画面表示するかどうか判定する処理を入れたので、アプリのmain機能をmainControllerにて処理を実施する形に変更

--- Resources/app.js
+++ Resources/app.js
@@ -1,56 +1,15 @@
-var Cloud, ListWindow, MapWindow, MypageWindow, listTab, listWindow, mapTab, mapWindow, mypageTab, mypageWindow, tabGroup;
+var Client, client, configurationWizard, mainController, startPoint;
 
-Cloud = require('ti.cloud');
+Ti.App.Properties.setBool("configurationWizard", false);
 
-tabGroup = Ti.UI.createTabGroup({
-  tabsBackgroundColor: "#f9f9f9",
-  shadowImage: "ui/image/shadowimage.png",
-  tabsBackgroundImage: "ui/image/tabbar.png",
-  activeTabBackgroundImage: "ui/image/activetab.png",
-  activeTabIconTint: "#fffBD5"
-});
+configurationWizard = Ti.App.Properties.getBool("configurationWizard");
 
-tabGroup.addEventListener('focus', function(e) {
-  tabGroup._activeTab = e.tab;
-  tabGroup._activeTabIndex = e.index;
-  if (tabGroup._activeTabIndex === -1) {
-    return;
-  }
-  Ti.API._activeTab = tabGroup._activeTab;
-  Ti.API.info(tabGroup._activeTab);
-});
-
-MapWindow = require("ui/mapWindow");
-
-mapWindow = new MapWindow();
-
-mapTab = Titanium.UI.createTab({
-  window: mapWindow,
-  icon: "ui/image/light_pin.png"
-});
-
-MypageWindow = require("ui/mypageWindow");
-
-mypageWindow = new MypageWindow();
-
-mypageTab = Titanium.UI.createTab({
-  window: mypageWindow,
-  icon: "ui/image/light_gears.png"
-});
-
-ListWindow = require("ui/listWindow");
-
-listWindow = new ListWindow();
-
-listTab = Titanium.UI.createTab({
-  window: listWindow,
-  icon: "ui/image/light_list.png"
-});
-
-tabGroup.addTab(mapTab);
-
-tabGroup.addTab(listTab);
-
-tabGroup.addTab(mypageTab);
-
-tabGroup.open();
+if (configurationWizard === true) {
+  Client = require("configurationWizard/client");
+  client = new Client();
+  startPoint = 0;
+  client.useMenu(startPoint);
+} else {
+  mainController = require("controller/mainController");
+  new mainController();
+}
--- Resources/controller/mainController.js
+++ Resources/controller/mainController.js
@@ -3,16 +3,47 @@ var mainController;
 mainController = (function() {
 
   function mainController() {
-    var ShopDataDetailWindow;
-    ShopDataDetailWindow = require("ui/shopDataDetailWindow");
-    this.shopDataDetailWindow = new ShopDataDetailWindow();
+    var Cloud, ListWindow, MapWindow, MypageWindow, listTab, listWindow, mapTab, mapWindow, mypageTab, mypageWindow, tabGroup;
+    Cloud = require('ti.cloud');
+    tabGroup = Ti.UI.createTabGroup({
+      tabsBackgroundColor: "#f9f9f9",
+      shadowImage: "ui/image/shadowimage.png",
+      tabsBackgroundImage: "ui/image/tabbar.png",
+      activeTabBackgroundImage: "ui/image/activetab.png",
+      activeTabIconTint: "#fffBD5"
+    });
+    tabGroup.addEventListener('focus', function(e) {
+      tabGroup._activeTab = e.tab;
+      tabGroup._activeTabIndex = e.index;
+      if (tabGroup._activeTabIndex === -1) {
+        return;
+      }
+      Ti.API._activeTab = tabGroup._activeTab;
+      Ti.API.info(tabGroup._activeTab);
+    });
+    MapWindow = require("ui/mapWindow");
+    mapWindow = new MapWindow();
+    mapTab = Titanium.UI.createTab({
+      window: mapWindow,
+      icon: "ui/image/light_pin.png"
+    });
+    MypageWindow = require("ui/mypageWindow");
+    mypageWindow = new MypageWindow();
+    mypageTab = Titanium.UI.createTab({
+      window: mypageWindow,
+      icon: "ui/image/light_gears.png"
+    });
+    ListWindow = require("ui/listWindow");
+    listWindow = new ListWindow();
+    listTab = Titanium.UI.createTab({
+      window: listWindow,
+      icon: "ui/image/light_list.png"
+    });
+    tabGroup.addTab(mapTab);
+    tabGroup.addTab(listTab);
+    tabGroup.addTab(mypageTab);
+    tabGroup.open();
   }
-
-  mainController.prototype.updateShopDataDetailWindow = function(data) {
-    return this.shopDataDetailWindow.update(data);
-  };
-
-  mainController.prototype.findNearBy = function() {};
 
   return mainController;
 
