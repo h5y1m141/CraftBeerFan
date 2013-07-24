dbf993d25f505e69ebd6d3dfb912c0b0ed1809e5
hiroshi oyamada <h5y1m141@gmail.com>
2013-07-25 06:03:04 +0900

    GoogleAnalyticsによるトラッキングするために外部モジュール読み込み

--- Resources/app.js
+++ Resources/app.js
@@ -1,8 +1,38 @@
-var Client, client, configurationWizard, mainController, startPoint;
+var Client, Config, analytics, client, config, configurationWizard, gaKey, gaModule, mainController, startPoint;
 
 configurationWizard = Ti.App.Properties.getBool("configurationWizard");
 
-Ti.API.info(configurationWizard);
+Config = require("model/loadConfig");
+
+config = new Config();
+
+gaKey = config.getGoogleAnalyticsKey();
+
+gaModule = require('lib/Ti.Google.Analytics');
+
+analytics = new gaModule(gaKey);
+
+Ti.App.addEventListener("analytics_trackEvent", function(e) {
+  return analytics.trackEvent(e.category, e.action, e.label, e.value);
+});
+
+Ti.App.Analytics = {
+  trackPageview: function(pageUrl) {
+    return Ti.App.fireEvent("analytics_trackPageview", {
+      pageUrl: pageUrl
+    });
+  },
+  trackEvent: function(category, action, label, value) {
+    return Ti.App.fireEvent("analytics_trackEvent", {
+      category: category,
+      action: action,
+      label: label,
+      value: value
+    });
+  }
+};
+
+analytics.start(10, true);
 
 if (configurationWizard === null || typeof configurationWizard === "undefined") {
   Client = require("configurationWizard/client");
--- coffee/app.coffee
+++ coffee/app.coffee
@@ -2,8 +2,32 @@
 
 configurationWizard = Ti.App.Properties.getBool "configurationWizard"
 
-Ti.API.info configurationWizard
+# GoogleAnalyticsによるトラッキングのための処理
 
+Config = require("model/loadConfig")
+config = new Config()
+gaKey = config.getGoogleAnalyticsKey()
+
+gaModule = require('lib/Ti.Google.Analytics')
+analytics = new gaModule(gaKey)
+Ti.App.addEventListener "analytics_trackEvent", (e) ->
+  analytics.trackEvent e.category, e.action, e.label, e.value
+
+Ti.App.Analytics =
+  trackPageview: (pageUrl) ->
+    Ti.App.fireEvent "analytics_trackPageview",
+      pageUrl: pageUrl
+
+
+  trackEvent: (category, action, label, value) ->
+    Ti.App.fireEvent "analytics_trackEvent",
+      category: category
+      action: action
+      label: label
+      value: value
+
+
+analytics.start 10, true
 if configurationWizard is null or typeof configurationWizard is "undefined"
   Client = require("configurationWizard/client")
   client = new Client()
