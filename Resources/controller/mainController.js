fa39e94a42fb4f099584ed3ddce6b4aedb66585c
hiroshi oyamada <h5y1m141@gmail.com>
2013-07-26 07:22:18 +0900

    どのタブに画面遷移しているのかトラッキング出来る機能を実装

--- Resources/controller/mainController.js
+++ Resources/controller/mainController.js
@@ -19,25 +19,28 @@ mainController = (function() {
         return;
       }
       Ti.API._activeTab = tabGroup._activeTab;
-      Ti.API.info(tabGroup._activeTab);
+      Ti.App.Analytics.trackPageview("/tab/" + tabGroup._activeTab.windowName);
     });
     MapWindow = require("ui/mapWindow");
     mapWindow = new MapWindow();
     mapTab = Titanium.UI.createTab({
       window: mapWindow,
-      icon: "ui/image/light_pin.png"
+      icon: "ui/image/light_pin.png",
+      windowName: "mapWindow"
     });
     MypageWindow = require("ui/mypageWindow");
     mypageWindow = new MypageWindow();
     mypageTab = Titanium.UI.createTab({
       window: mypageWindow,
-      icon: "ui/image/light_gears.png"
+      icon: "ui/image/light_gears.png",
+      windowName: "mypageWindow"
     });
     ListWindow = require("ui/listWindow");
     listWindow = new ListWindow();
     listTab = Titanium.UI.createTab({
       window: listWindow,
-      icon: "ui/image/light_list.png"
+      icon: "ui/image/light_list.png",
+      windowName: "listWindow"
     });
     tabGroup.addTab(mapTab);
     tabGroup.addTab(listTab);
--- coffee/controller/mainController.coffee
+++ coffee/controller/mainController.coffee
@@ -16,7 +16,11 @@ class mainController
         return
 
       Ti.API._activeTab = tabGroup._activeTab;
-      Ti.API.info tabGroup._activeTab
+
+      # ページビューを取得したいので以下を参考にしてイベントリスナー設定  
+      # http://hirofukami.com/2013/05/31/titanium-google-analytics/
+
+      Ti.App.Analytics.trackPageview "/tab/#{tabGroup._activeTab.windowName}"
       return
     )
 
@@ -25,12 +29,14 @@ class mainController
     mapTab = Titanium.UI.createTab
       window:mapWindow
       icon:"ui/image/light_pin.png"
+      windowName:"mapWindow"
 
     MypageWindow = require("ui/mypageWindow")
     mypageWindow = new MypageWindow()
     mypageTab = Titanium.UI.createTab
       window:mypageWindow
       icon:"ui/image/light_gears.png"
+      windowName:"mypageWindow"      
 
 
 
@@ -39,7 +45,7 @@ class mainController
     listTab = Titanium.UI.createTab
       window:listWindow
       icon:"ui/image/light_list.png"
-
+      windowName:"listWindow"      
 
     tabGroup.addTab mapTab
     tabGroup.addTab listTab
